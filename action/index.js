951146a370410b0f60d7b40a8465a1c77a8272d0
Upkar Lidder <ulidder@us.ibm.com>
2021-08-03 16:16:18 -0700

    formatting files and adding linter github action
    Signed-off-by: Upkar Lidder <ulidder@us.ibm.com>

--- action/index.js
+++ action/index.js
@@ -1,104 +1,116 @@
-const logger = require('pino')();
-const fs = require('fs');
-var mqtt = require('mqtt');
+const logger = require("pino")();
+const fs = require("fs");
+var mqtt = require("mqtt");
 
 function main(params) {
-    return new Promise((resolve, reject) => {
-        var pemFile = fs.readFileSync(params.IOT_PEM);
-        // the clientid format from the docs: d:orgId:deviceType:deviceId. 
-        // const clientID = params.IOT_CLIENTID + `-${Date.now()}`;
-        const options = {
-            host: params.IOT_HOST,
-            protocol: params.IOT_PROTOCOL,
-            username: params.IOT_USERNAME,
-            port: params.IOT_SECURE_PORT,
-            password: params.IOT_PASSWORD,
-            clientId: params.IOT_CLIENTID,
-            cert: pemFile
-        };
-        
-        const client = mqtt.connect(options);
+  return new Promise((resolve, reject) => {
+    var pemFile = fs.readFileSync(params.IOT_PEM);
+    // the clientid format from the docs: d:orgId:deviceType:deviceId.
+    // const clientID = params.IOT_CLIENTID + `-${Date.now()}`;
+    const options = {
+      host: params.IOT_HOST,
+      protocol: params.IOT_PROTOCOL,
+      username: params.IOT_USERNAME,
+      port: params.IOT_SECURE_PORT,
+      password: params.IOT_PASSWORD,
+      clientId: params.IOT_CLIENTID,
+      cert: pemFile,
+    };
 
-        client.on('connect', function (err) {
+    const client = mqtt.connect(options);
 
-            if(err){
-                console.log(`error connecting ${params.IOT_DEVICE_ID}`)
-                console.log(JSON.stringify(err));
-            }
-            logger.info('connnected and ready to publish!');
-            // publish(client, params);
+    client.on("connect", function (err) {
+      if (err) {
+        console.log(`error connecting ${params.IOT_DEVICE_ID}`);
+        console.log(JSON.stringify(err));
+      }
+      logger.info("connnected and ready to publish!");
+      // publish(client, params);
 
-            const data = {
-                "firefighter_id": params.IOT_FIREFIGHTER_ID,
-                "device_id": params.IOT_DEVICE_ID,
-                "device_battery_level": (Math.random() * (0.00 - 100.00) + 0.0200).toFixed(2),
-                "temperature": (Math.random() * 50).toFixed(2),
-                "humidity": (Math.random() * 100).toFixed(2),
-                "carbon_monoxide": (Math.random() * 150).toFixed(2),
-                "nitrogen_dioxide": (Math.random() * 10).toFixed(2),
-                "formaldehyde": (Math.random() * 10).toFixed(2),
-                "acrolein": (Math.random() * 10).toFixed(2),
-                "benzene": (Math.random() * 10).toFixed(2),
-                "device_timestamp": getUTCTime()
-            }
+      const data = {
+        firefighter_id: params.IOT_FIREFIGHTER_ID,
+        device_id: params.IOT_DEVICE_ID,
+        device_battery_level: (Math.random() * (0.0 - 100.0) + 0.02).toFixed(2),
+        temperature: (Math.random() * 50).toFixed(2),
+        humidity: (Math.random() * 100).toFixed(2),
+        carbon_monoxide: (Math.random() * 150).toFixed(2),
+        nitrogen_dioxide: (Math.random() * 10).toFixed(2),
+        formaldehyde: (Math.random() * 10).toFixed(2),
+        acrolein: (Math.random() * 10).toFixed(2),
+        benzene: (Math.random() * 10).toFixed(2),
+        device_timestamp: getUTCTime(),
+      };
 
-            const topic = `iot-2/evt/myevt/fmt/json`;
+      const topic = `iot-2/evt/myevt/fmt/json`;
 
-            client.publish(topic, JSON.stringify(data), (err) => {
-                if (err) {
-                    logger.error(err);
-                    reject({ err: err });
-                } else {
-                    logger.info(`\n\n\nfinished publishing data: ${JSON.stringify(data)}`);
-                }
+      client.publish(topic, JSON.stringify(data), (err) => {
+        if (err) {
+          logger.error(err);
+          reject({ err: err });
+        } else {
+          logger.info(
+            `\n\n\nfinished publishing data: ${JSON.stringify(data)}`
+          );
+        }
 
-                client.end();
-                resolve({ msg: "published data" });
-            });
+        client.end();
+        resolve({ msg: "published data" });
+      });
+    });
 
-        });
+    client.on("message", onMessage);
 
-        client.on('message', onMessage)
+    client.on("close", onClose);
 
-        client.on('close', onClose)
+    client.on("disconnect", onDisconnect);
 
-        client.on('disconnect', onDisconnect)
-
-        client.on('error', onError)
-
-    })
+    client.on("error", onError);
+  });
 }
 
 function onMessage(topic, message) {
-    logger.info('\n\n\n\n');
-    logger.info(message.toString())
+  logger.info("\n\n\n\n");
+  logger.info(message.toString());
 }
 
 function onClose(err) {
-    if (err) { logger.info(err) };
-    logger.info('connection closed');
+  if (err) {
+    logger.info(err);
+  }
+  logger.info("connection closed");
 }
 
 function onDisconnect(err) {
-    logger.info('connection disconnected');
+  logger.info("connection disconnected");
 }
 
 function onError(err) {
-    logger.error('connection error');
-    logger.error(err);
+  logger.error("connection error");
+  logger.error(err);
 }
 
 function getUTCTime() {
-    var dateObj = new Date();
-    var month = dateObj.getUTCMonth() + 1; //months from 1-12
-    var day = dateObj.getUTCDate();
-    var year = dateObj.getUTCFullYear();
-    var hours = dateObj.getUTCHours();
-    var minutes = dateObj.getUTCMinutes();
-    var seconds = dateObj.getUTCSeconds();
+  var dateObj = new Date();
+  var month = dateObj.getUTCMonth() + 1; //months from 1-12
+  var day = dateObj.getUTCDate();
+  var year = dateObj.getUTCFullYear();
+  var hours = dateObj.getUTCHours();
+  var minutes = dateObj.getUTCMinutes();
+  var seconds = dateObj.getUTCSeconds();
 
-    var newdate = year + "-" + month + "-" + day + " " + hours + ":" + minutes + ":" + seconds;
-    return newdate;
+  var newdate =
+    year +
+    "-" +
+    month +
+    "-" +
+    day +
+    " " +
+    hours +
+    ":" +
+    minutes +
+    ":" +
+    seconds;
+  return newdate;
 }
 
 exports.main = main;
--- action/server.js
+++ action/server.js
@@ -1,38 +1,39 @@
-const server = require('./index')
-require('dotenv').config();
-const deviceFile = require('./devices.json')
+const server = require("./index");
+require("dotenv").config();
+const deviceFile = require("./devices.json");
 
 const devices = deviceFile.devices;
 
-var cron = require('node-cron');
+var cron = require("node-cron");
 
 console.log(process.env.IOT_SECURE_PORT);
 
-devices.forEach(device => {
-    console.log(`Found device: ${device.IOT_DEVICE_ID}`);
+devices.forEach((device) => {
+  console.log(`Found device: ${device.IOT_DEVICE_ID}`);
 });
 
-cron.schedule('* * * * *', () => {
-    console.log('running a task every minute');
+cron.schedule("* * * * *", () => {
+  console.log("running a task every minute");
 
-    devices.forEach(device => {
-        let params = {
-            IOT_HOST: process.env.IOT_HOST,
-            IOT_PEM: process.env.IOT_PEM,
-            IOT_PROTOCOL: process.env.IOT_PROTOCOL,
-            IOT_USERNAME: process.env.IOT_USERNAME,
-            IOT_SECURE_PORT: process.env.IOT_SECURE_PORT,
-            IOT_PASSWORD: device.IOT_PASSWORD,
-            IOT_CLIENTID: device.IOT_CLIENTID,
-            IOT_FIREFIGHTER_ID: device.IOT_FIREFIGHTER_ID,
-            IOT_DEVICE_ID: device.IOT_DEVICE_ID
-        };
-        server.main(params)
-            .then(result => {
-                console.log(result);
-            })
-            .catch(err => {
-                console.log(err);
-            });
-    });
+  devices.forEach((device) => {
+    let params = {
+      IOT_HOST: process.env.IOT_HOST,
+      IOT_PEM: process.env.IOT_PEM,
+      IOT_PROTOCOL: process.env.IOT_PROTOCOL,
+      IOT_USERNAME: process.env.IOT_USERNAME,
+      IOT_SECURE_PORT: process.env.IOT_SECURE_PORT,
+      IOT_PASSWORD: device.IOT_PASSWORD,
+      IOT_CLIENTID: device.IOT_CLIENTID,
+      IOT_FIREFIGHTER_ID: device.IOT_FIREFIGHTER_ID,
+      IOT_DEVICE_ID: device.IOT_DEVICE_ID,
+    };
+    server
+      .main(params)
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
 });
